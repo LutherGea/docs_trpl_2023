f4e31f94c9fd5f7ea240f3d4c872d8b3a421ac3d
LutherGea <luthergea2004@gmail.com>
2025-01-19 00:58:49 +0700

    dfbvgnwjre

--- app/page.tsx
+++ app/page.tsx
@@ -7,10 +7,10 @@ export default function Home() {
   return (
     <div className="min-h-[86.5vh] flex flex-col justify-center items-center text-center px-2 py-8">
       <h1 className="text-4xl font-bold mb-4 sm:text-7xl">
-        Documents kasir laundry
+        Kasir Laundry
       </h1>
       <p className="max-w-[600px] text-foreground mb-8 sm:text-base">
-      kasir laundry adalah platform berbasis web yang dirancang untuk mendukung operasional bisnis laundry, khususnya dalam mengelola transaksi dan layanan pelanggan.
+      
       </p>
       <div className="flex items-center gap-5">
         <Link
